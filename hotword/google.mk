32b98220c5fc32e7cdd8fb86d47d44388cc87ac6
neobuddy89 <neobuddy89@gmail.com>
2023-01-23 09:34:17 +0000

    [BLOBS] sm7125-common: Import hotword enrollment
    Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>

---
+++ hotword/google.mk
@@ -0,0 +1,12 @@
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/realme/hotword
+
+PRODUCT_COPY_FILES += \
+    vendor/realme/hotword/proprietary/system_ext/etc/permissions/com.android.hotwordenrollment.common.util.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.android.hotwordenrollment.common.util.xml \
+    vendor/realme/hotword/proprietary/system_ext/framework/com.android.hotwordenrollment.common.util.jar:$(TARGET_COPY_OUT_SYSTEM_EXT)/framework/com.android.hotwordenrollment.common.util.jar \
+    vendor/realme/hotword/proprietary/product/etc/permissions/privapp-permissions-google-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-google-hotword.xml \
+    vendor/realme/hotword/proprietary/product/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml
+
+PRODUCT_PACKAGES += \
+    HotwordEnrollmentOKGoogleHEXAGON \
+    HotwordEnrollmentXGoogleHEXAGON
